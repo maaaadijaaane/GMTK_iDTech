decb06af2a23cb134c0ec0eff3cf8050bc453843
maaaadijaaane <madison.holcomb@live.com>
2022-07-16 00:28:16 -0700

    Organize blocks using DroppedBlocks empty GameObject
    Parenting generated blocks into DroppedBlocks and "Name of Block" GameObject. Ex. ToyBlock, ToyColumn

--- Assets/BlockFactory.cs
+++ Assets/BlockFactory.cs
@@ -7,11 +7,19 @@ public class BlockFactory : MonoBehaviour
     public GameObject block; // Need to store prefab of block you want to generate here
     private GameObject newBlock;
     public static List<GameObject> blocks = new List<GameObject>();
+    private GameObject droppedBlocks;
+    private GameObject theseBlocks;
 
+    void Start()
+    {
+        droppedBlocks = GameObject.Find("DroppedBlocks");
+        theseBlocks = new GameObject(block.name + "Objects");
+        theseBlocks.transform.SetParent(droppedBlocks.gameObject.transform);
+    }
     public void AddBlock()
     {
-        newBlock = new GameObject();
         newBlock = Instantiate(block, transform.position, transform.rotation);
+        newBlock.transform.SetParent(theseBlocks.gameObject.transform);
         blocks.Add(newBlock);
     }
 }
