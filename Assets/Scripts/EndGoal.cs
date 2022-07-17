1d2aa764825eb71dffaedbb7cb3c9b86c1fe60e2
ZoeWheatcroft <zoe.wheatcroft@gmail.com>
2022-07-17 09:49:24 -0700

    End goal + End screen

---
+++ Assets/Scripts/EndGoal.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGoal : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("game won");
+            SceneManager.LoadScene("EndScene");
+        }
+    }
+
+}
